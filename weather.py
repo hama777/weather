92905764ebba3303c725792f4042c271320a7453
hama777 <GEB01604@nifty.ne.jp>
2024-10-04 18:49:43 +0900

    24/10/04 v0.01

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ import codecs
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-version = "0.00"     # 24/10/03 v0.00  開発開始
+version = "0.01"     # 24/10/04 v0.01  
 out =  ""
 logf = ""
 appdir = os.path.dirname(os.path.abspath(__file__))
@@ -32,9 +32,17 @@ def access_site() :
 def analize() :
     top = BeautifulSoup(res.text, 'html.parser')
     div_1hour = top.find('div', id ='flick_list_1hour')
-    #div_1hour = top.find_all(id ='flick_list_1hour')
-    div_date = div_1hour.find('div', class_ ='date')
+    div_date = div_1hour.find('div', class_ ='date')    # 最初に出てくる  日
     print(div_date.text)
+    
+    time_data = div_1hour.find('li', class_ ='time')    # 最初に出てくる  時
+    print(time_data.text)
+
+    weather_items = div_1hour.find_all('li', class_ ='weather')
+    for w in weather_items :
+        img = w.find('img' ,class_ = "wx__icon")
+        icon = img.get('src')
+        print(icon)
 
 def read_config() : 
     global target_url,proxy,debug
