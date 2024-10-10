9fbc04c81a6e3e6779bc1982655b7cca9edcd7e3
hama777 <GEB01604@nifty.ne.jp>
2024-10-10 19:03:44 +0900

    24/10/10 v0.00 htmlファイル出力

---
+++ view.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import re
+import datetime
+from datetime import date,timedelta
+#from datetime import datetime
+
+# 24/10/10 v0.00 htmlファイル出力
+version = "0.00"     
+
+out =  ""
+logf = ""
+appdir = os.path.dirname(os.path.abspath(__file__))
+datafile = appdir + "/data/we.txt" 
+outfile = appdir + "/weather.htm" 
+conffile = appdir + "/weather.conf"
+res = ""
+
+def main_proc() :
+    #read_config()
+    #output_datafile()
+    #output_result()
+
+    read_data()
+    output_html()
+
+def read_data() : 
+    global start_date,start_hh,we_list
+    f = open(datafile , 'r')
+    header  = f.readline().strip()
+    hh = header.split()
+    date_str = hh[0]
+    dt = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    start_date = datetime.date(dt.year, dt.month, dt.day)
+
+    start_hh = int(hh[1])
+    body  = f.readline().strip()
+    we_list = body.split(",")
+    #print(we_list)
+    f.close()
+
+def output_html() :
+    
+    out = open(outfile , 'w', encoding='utf-8')
+
+    out.write("<table><tr><th>日付</th><th>時間</th><th>天気</th></tr>\n")
+    cur_date = start_date
+    cur_hh = start_hh
+    for we in  we_list :
+        out.write(f'<tr><td>{cur_date}</td><td>{cur_hh}</td><td>'
+        f'<img src="https://weathernews.jp/onebox/img/wxicon/{we}.png" width="40" height="30"></td></tr>\n')
+        cur_hh += 1
+        if cur_hh == 24 :
+            cur_hh = 0
+            cur_date +=  datetime.timedelta(days=1)
+
+    out.write("</table>\n")
+    out.close()
+
+def output_datafile() :
+    today_date = date.today()
+    cur_mm =  today_date.month    #  今月
+    cur_yy =  today_date.year    #  今月
+    start_date = datetime.date(cur_yy, cur_mm , start_dd)
+    out = open(outfile , 'w', encoding='utf-8')
+    s = str(start_date) + " " + str(start_hh) + "\n"
+    out.write(s)
+    s = ",".join(map(str, we_list))
+    out.write(s)
+    out.write("\n")
+    out.close()
+
+def output_result() :
+    today_date = date.today()
+    cur_mm =  today_date.month    #  今月
+    cur_yy =  today_date.year    #  今月
+    start_date = datetime.date(cur_yy, cur_mm , start_dd)
+    cur_date = start_date
+    cur_hh = start_hh
+    print(start_date)
+    for we in  we_list :
+        print(cur_date,cur_hh,we)
+        cur_hh += 1
+        if cur_hh == 24 :
+            cur_hh = 0
+            cur_date +=  datetime.timedelta(days=1)
+
+
+def read_config() : 
+    global target_url,proxy,debug
+    if not os.path.isfile(conffile) :
+        debug = 1 
+        return
+
+    conf = open(conffile,'r', encoding='utf-8')
+    target_url = conf.readline().strip()
+    proxy  = conf.readline().strip()
+    debug = int(conf.readline().strip())
+    conf.close()
+
+# -------------------------------------------------------------
+main_proc()
