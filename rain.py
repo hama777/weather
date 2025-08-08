fd8da051ca5be5b389903b1d41d5af51686af1d2
hama777 <GEB01604@nifty.ne.jp>
2025-08-08 20:19:39 +0900

    25/08/08 v1.13 降水量df作成処理追加

--- rain.py
+++ rain.py
@@ -8,8 +8,8 @@ import com
 from datetime import date,timedelta
 from ftplib import FTP_TLS
 
-# 25/06/25 v1.11 連続晴時間Topを表示
-version = "1.11"
+# 25/08/08 v1.13 降水量df作成処理追加
+version = "1.13"
 
 out =  ""
 logf = ""
@@ -22,8 +22,33 @@ templatefile = appdir + "/weather_templ.htm"
 temperafile = appdir + "/temperature.txt"    #  実績気温データ  
 dailyfile = appdir + "/dailyinfo.txt"
 act_weather_file = appdir + "/actweather.txt"   #  実績天気データ  1時間ごと
+prec_file = appdir + "/precipitation.txt"     #  降水量データ
 
 df_week_rain = ""
+
+def preprocess() :
+    create_df_week_rain()
+    continuous_fine_rain()
+    create_df_prec()
+
+#   降水量のdfを作成
+#         df_prec    date prec
+def create_df_prec() :
+    global df_prec
+    date_list = [] 
+    prec_list = []
+    with open(prec_file , encoding='utf-8') as f:
+        for line in f:
+            dt = line.split("\t")
+            date_str = dt[0] 
+            prec = float(dt[1])
+            tdate = datetime.datetime.strptime(date_str, '%y/%m/%d %H:%M')            
+            date_list.append(tdate)
+            prec_list.append(prec)
+
+    df_prec = pd.DataFrame(list(zip(date_list,prec_list)), columns = ['pdate','prec'])
+    print(df_prec)
+
 
 #  週間雨時間移動平均 df_week_rain の作成
 def create_df_week_rain() :
@@ -63,9 +88,7 @@ def week_rain_time_graph(out) :
 
 #   月別 1日平均雨時間テーブル
 def monthly_rain_time(out) :
-    #continuous_fine_rain()
     monthly_stats = df_daily_rain.resample('ME').agg({'rain': ['mean', 'max'],'is_rain' : 'sum'})
-    #print(monthly_stats)
     for index,row in monthly_stats.iterrows() :
         ave = row['rain']['mean']
         max = row['rain']['max']
@@ -74,10 +97,8 @@ def monthly_rain_time(out) :
         out.write(f'<tr><td>{date_str}</td><td align="right">{ave:5.2f}</td>'
                   f'<td align="right">{max}</td><td align="right">{days_rain:4.0f}</td></tr>')
 
-#   連続雨時間解析
+#   連続雨時間解析  df 作成
 def continuous_fine_rain() :
-    global rain_date_list,rain_con_list
-    global fine_date_list,fine_con_list
     global cur_continuous_data 
     global df_cont_rain,df_cont_fine
     cur_continuous_data = {}    # 現在の連続天気情報   key date , count , rain(雨の時 1)  
@@ -112,7 +133,7 @@ def continuous_fine_rain() :
         cur_continuous_data['date'] = date_str
         cur_continuous_data['count'] = rain_con
         cur_continuous_data['rain'] = 1
-        rain_date_list.append(date_str)
+        rain_date_list.append(int(date_str))
         rain_con_list.append(rain_con)
 
     if fine_con != 0 :
@@ -122,25 +143,33 @@ def continuous_fine_rain() :
         fine_date_list.append(date_str)
         fine_con_list.append(fine_con)
 
-    #print(cur_continuous_data)
-    #print(rain_date_list,rain_con_list)
-    #print(fine_date_list,fine_con_list)
-
     df_cont_rain = pd.DataFrame(list(zip(rain_date_list,rain_con_list)), columns = ['yymmddhh','cont'])
     df_cont_fine = pd.DataFrame(list(zip(fine_date_list,fine_con_list)), columns = ['yymmddhh','cont'])
+    df_cont_rain['yymmddhh'] = df_cont_rain['yymmddhh'].astype(int)
+    df_cont_fine['yymmddhh'] = df_cont_rain['yymmddhh'].astype(int)
     #print(df_cont_rain)
 
+#   連続雨時間表示
 def continuous_rain(out) :
     last = 20    # 直近、n件のみ表示する
-    for rdate , rtime in zip(rain_date_list[-last:],rain_con_list[-last:]) :
-        # if rtime <= 3 :   #  連続3時間以内は無視
-        #     continue
-        yymmddhh  = int(rdate)
+    continuous_com(out,df_cont_rain.tail(last))
+
+def continuous_fine(out) :
+    last = 20
+    continuous_com(out,df_cont_fine.tail(last))
+
+def continuous_com(out,df) :
+    for index , row in df.iterrows()  :
+        yymmddhh  = row['yymmddhh']
+        cont = row['cont']
         dt = com.conv_mmddhh_to_date(yymmddhh)
         hh = yymmddhh % 100
         date_str = dt.strftime('%m/%d (%a)')
-        out.write(f'<tr><td>{date_str} {hh:02}時</td><td align="right">{rtime}</td></tr>\n')
+        days = cont // 24
+        cont_hh = cont % 24
+        out.write(f'<tr><td>{date_str} {hh:02}時</td><td align="right">{cont}({days}日{cont_hh:02}時間)</td></tr>\n')
 
+#   連続雨時間Top表示
 def top_continuous_rain(out) :
     df_top = df_cont_rain.sort_values('cont',ascending=False)
     top_continuous_com(out,df_top) 
@@ -158,22 +187,9 @@ def top_continuous_com(out,df_top) :
         count = row['cont']
         days = count // 24
         fine_hh = count % 24
-
         out.write(f'<tr><td>{date_str} {hh:02}時</td><td align="right">{count}({days:2}日{fine_hh:02}時間)</td></tr>\n')
 
-def continuous_fine(out) :
-    last = 20
-    for rdate , rtime in zip(fine_date_list[-last:],fine_con_list[-last:]) :
-        # if rtime <= 24 :  #  連続24時間以内は無視
-        #     continue
-        yymmddhh  = int(rdate)
-        dt = com.conv_mmddhh_to_date(yymmddhh)
-        hh = yymmddhh % 100
-        date_str = dt.strftime('%m/%d (%a)')
-        days = rtime // 24
-        fine_hh = rtime % 24
-        out.write(f'<tr><td>{date_str} {hh:02}時</td><td align="right">{rtime}({days}日{fine_hh:02}時間)</td></tr>\n')
-
+#   現時点での連続時間を表示
 def cur_continuous(out) :
     if cur_continuous_data['rain'] == 0 :
         s = "連続晴情報 "
@@ -184,4 +200,3 @@ def cur_continuous(out) :
     hh = yymmddhh % 100
     date_str = dt.strftime('%m/%d (%a)')
     out.write(f"{s} {cur_continuous_data['count']} 時間 {date_str} {hh}時 現在\n")
-
